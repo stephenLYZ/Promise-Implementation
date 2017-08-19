bcb2fd8ed690381f109337412c669773fbbb56f7
stephenliu <stephenlyao@yeah.net>
2017-08-19 23:20:25 +0800

    add constructor

--- src/Aromise.js
+++ src/Aromise.js
@@ -1,3 +1,14 @@
-class Aromise {
-  
+const PENDING = Symbol()
+const FULFILLED = Symbol()
+const REJECTED = Symbol()
+
+export default class Aromise {
+  constructor(fn) {
+    if (typeof fn !== 'function') {
+      throw new Error('resolver should be a function')
+    }
+    this.state = PENDING
+    this.value = null
+    this.handlers = []
+  }
 }
